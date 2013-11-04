ed7b71ecd8d6dfe1a9b1ae77961f87f159a3a138
abdelkader boudih <terminale@gmail.com>
2013-11-04 12:56:39 +0000

    updated to work with rails 4 and changed migration to accept non-integer ids

--- lib/generators/acts_as_friendable/install/templates/friendships.rb
+++ lib/generators/acts_as_friendable/install/templates/friendships.rb
@@ -1,11 +1,12 @@
 class CreateActsAsFriendableFriendships < ActiveRecord::Migration
   def change
     create_table :friendships do |t|
-      t.integer :user_id
-      t.integer :friend_id
+      t.uuid :user_id
+      t.uuid :friend_id
       t.boolean :approved
 
       t.timestamps
     end
+
   end
 end
